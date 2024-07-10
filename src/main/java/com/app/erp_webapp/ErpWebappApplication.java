08be57b9cfa2d2738a948ec37204e619b178002a
danielsfilho <danielbrumdasilva@gmail.com>
2024-07-10 00:23:21 -0300

    chore: first configuration with database

---
+++ src/main/java/com/app/erp_webapp/ErpWebappApplication.java
@@ -0,0 +1,13 @@
+package com.app.erp_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/erp_webapp/ErpWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.erp_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
